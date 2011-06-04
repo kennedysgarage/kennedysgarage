9543702d4f83cad3df545cd050ec0a739b4e0f7a
Christopher Kennedy <kennedysgarage@gmail.com>
2011-06-04 19:29:03 -0400

    testing to see if the readablity buttons will work if i move it to an external js file

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
 /* Author: 
-	google.com/analytics
+	
 */
-function googleCode(){
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-17557746-1']);
-	_gaq.push(['_setDomainName', '.kennedysgarage.com']);
-	_gaq.push(['_trackPageview']);
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-}
-$(document).ready(function() {
-	googleCode();
-});
-
-/* Author: 
-	readability.com
-*/
-(function() {
+    (function() {
     var s     = document.getElementsByTagName('script')[0],
         rdb   = document.createElement('script');
     rdb.type  = 'text/javascript';
