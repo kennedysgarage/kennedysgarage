464a666247c745580e7aa5fb2efd8094f3a87e9e
Christopher Kennedy <kennedysgarage@gmail.com>
2011-06-06 10:37:11 -0400

    stupid float

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /* Author: 
 	
 */
-    (function() {
-    var s     = document.getElementsByTagName('script')[0],
-        rdb   = document.createElement('script');
-    rdb.type  = 'text/javascript';
-    rdb.async = true;
-    rdb.src   = document.location.protocol + '//www.readability.com/embed.js';
-    s.parentNode.insertBefore(rdb, s);
-    })();
+(function() {
+var s     = document.getElementsByTagName('script')[0],
+    rdb   = document.createElement('script');
+rdb.type  = 'text/javascript';
+rdb.async = true;
+rdb.src   = document.location.protocol + '//www.readability.com/embed.js';
+s.parentNode.insertBefore(rdb, s);
+})();
