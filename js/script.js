0959d8fa471708a91298dbf7c69e6fe1fc16e65a
Christopher Kennedy <kennedysgarage@gmail.com>
2011-07-03 19:10:28 -0400

    fixed my js for disqus

--- js/script.js
+++ js/script.js
@@ -9,19 +9,3 @@ rdb.async = true;
 rdb.src   = document.location.protocol + '//www.readability.com/embed.js';
 s.parentNode.insertBefore(rdb, s);
 })();
-
-/*
-	disqus.com
-*/
-var disqus_shortname = 'kennedysgarage';
-
-// The following are highly recommended additional parameters. Remove the slashes in front to use.
-// var disqus_identifier = 'unique_dynamic_id_1234';
-// var disqus_url = 'http://example.com/permalink-to-page.html';
-
-/* * * DON'T EDIT BELOW THIS LINE * * */
-(function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
