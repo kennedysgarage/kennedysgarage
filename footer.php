f4cd5ea4525f16f19fb499beb52b276d1f061217
Christopher Kennedy <kennedysgarage@gmail.com>
2012-02-28 22:40:37 -0500

    cleaning up my js

--- footer.php
+++ footer.php
@@ -8,7 +8,6 @@
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/jquery-1.7.0.min.js">\x3C/script>')</script>
-<script src="/js/twipsy.js"></script>
 <script src="/js/prettify.js"></script>
 <script src="/js/script.js"></script>
 <!--[if lt IE 7 ]>
