c8ca30ddfa5d4f100d9d9b2d054bedc2e65fef2d
Christopher Kennedy <kennedysgarage@gmail.com>
2012-02-28 22:40:15 -0500

    cleaning up my js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
-	
-	$('a[rel=twipsy]').twipsy({
-		'placement': 'right',
-		'delayIn' : 400
-	});
-	
-    $('#tabs div').hide();
-    $('#tabs div:first').show();
-    $('#tabs ul li:first').addClass('active');
-    $('#tabs ul li a').click(function() {
-        $('#tabs ul li').removeClass('active');
-        $(this).parent().addClass('active');
-        var currentTab = $(this).attr('href');
-        $('#tabs div').hide();
-        $(currentTab).show();
-        return false;
-    });
-
+	// To make the code look beautiful for you 
+	// http://code.google.com/p/google-code-prettify/
 	prettyPrint();
 });
