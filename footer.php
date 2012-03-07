0ad4a2afc5cb0c6f6bea334b819e19fabe35ab77
Christopher Kennedy <kennedysgarage@gmail.com>
2012-03-06 22:00:08 -0500

    cleaning up minor

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 </footer>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/js/jquery-1.7.0.min.js">\x3C/script>')</script>
+<script>window.jQuery || document.write('<script src="/js/jquery.min.js">\x3C/script>')</script>
 <script src="/js/prettify.js"></script>
 <script src="/js/script.js"></script>
 <!--[if lt IE 7 ]>
