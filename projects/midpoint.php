8d4fb07d59d9fd88dcd68021a16b9dc98baec349
Christopher Kennedy <kennedysgarage@gmail.com>
2012-04-04 22:28:05 -0400

    added an American Express Open Forum video, oh yeah I am in it

--- projects/midpoint.php
+++ projects/midpoint.php
@@ -5,8 +5,9 @@
         <h2>Midpoint</h2>
         <p>I hacked on Midpoint at the <a href="http://generalassemb.ly/local-hack-day">Reinvent Local Hackathon</a> with <a href="http://twitter.com/DavidYKay">David Kay</a>. We build this as an Android app that allows you to find a great meeting spot. We used the <a href="http://hyperpublic.com">Hyperpublic</a> API to find local deals and possible meeting spots.</p>
         <img src="/projects/assets/midpoint/midpoint-900.png">
-        <br/><br/>
         <div class="video"><iframe width="950" height="513" src="http://www.youtube.com/embed/PLxAxMXWt6o?rel=0?wmode=opaque" frameborder="0" allowfullscreen></iframe></div>
+		<div class="video"><iframe width="950" height="513" src="http://www.youtube.com/embed/sf3mpcAp_GE?rel=0?wmode=opaque" frameborder="0" allowfullscreen></iframe></div>
+		<!-- I'm in there a few times: 1:12, 1:40, 2:52, 3:55 -->
 	</div><!--/content-->
 </div><!--/page-->
 
