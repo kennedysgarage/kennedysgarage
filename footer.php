aaef358f96d623a1af1dc50d66163c4fac324f9d
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-22 11:05:45 -0400

    forgot to add links

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
 <footer class="clearfix">
 	<div class="container">
 	<ul id="subnav">
-			<li><a href="">Email</a></li>
-			<li><a href="">Twitter</a></li>
-			<li><a href="">Google +</a></li>
-			<li><a href="">Dribbble</a></li>
-			<li><a href="">Forrst</a></li>
+			<li><a href="mailto:kennedysgarage@gmail.com">Email</a></li>
+			<li><a href="http://twitter.com/kennedysgarage">Twitter</a></li>
+			<li><a href="https://plus.google.com/115398388795575778115/posts">Google +</a></li>
+			<li><a href="http://dribbble.com/kennedysgarage">Dribbble</a></li>
+			<li><a href="http://forrst.com/people/kennedysgarage">Forrst</a></li>
 		</ul>
 		<h1>Kennedy's Garage</h1>
 		<p>My name is Christopher Kennedy and I am one of the <a href="http://www.betabeat.com/2012/04/09/betabeats-spring-2012-most-poachable-players-in-tech/#slide22">most poachable players in tech</a>. A New York City based professional hacker, UX Strategist & Mobile UI Designer. I like to stay very active in a wide range of startups, and I attend a lot hackathons. I just launched <a href="http://gradespotter.com">Grade Spotter</a>, a full-featured restaurant inspection guide for your iPhone. I am not currently taking any new projects at this time, but I am available for consulting.</p>
