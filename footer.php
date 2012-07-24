5fd47ecaa8c6c5007c9645601cc4267843ae6945
Christopher Kennedy <kennedysgarage@gmail.com>
2012-07-24 11:24:27 -0400

    fixing typo

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 	
 	<div class="g8">
 		<h1>Kennedy's Garage</h1>
-		<p>My name is Christopher Kennedy and I am one of the <a href="http://www.betabeat.com/2012/04/09/betabeats-spring-2012-most-poachable-players-in-tech/#slide22">most poachable players in tech</a>. A New York City based professional hacker, UX Strategist & Mobile UI Designer. I like to stay very active in a wide range of startups, and I attend a lot hackathons. I just launched <a href="http://gradespotter.com">Grade Spotter</a>, a full-featured restaurant inspection guide for your iPhone. I am not currently taking any new projects at this time.</p>
+		<p>My name is Christopher Kennedy and I am one of the <a href="http://www.betabeat.com/2012/04/09/betabeats-spring-2012-most-poachable-players-in-tech/#slide22">most poachable players in tech</a>. A New York City based professional hacker, UX Strategist & Mobile UI Designer. I like to stay very active in a wide range of startups, and I attend a lot of hackathons. I just launched <a href="http://gradespotter.com">Grade Spotter</a>, a full-featured restaurant inspection guide for your iPhone. I am not currently taking any new projects at this time.</p>
 	</div><!--/g8-->
 	<div class="g4">
 		<ul id="subnav">
