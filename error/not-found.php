fc99a20d43726aadbc6fcd0a90981f6561256338
Christopher Kennedy <kennedysgarage@gmail.com>
2012-07-24 23:55:02 -0400

    fixing path

--- error/not-found.php
+++ error/not-found.php
@@ -1,16 +1,12 @@
 <?php  $title = 'Not Found |'; include '../header.php'; ?>
 
-<div id="page">
-	<div id="content">
+<div class="g12">
 			
-			<h2>Not Found</h2>
-			<p>Sorry, but the page you were trying to view does not exist. Please allow Bud Haggart to explain.</p>
-			
-			<iframe width="950" height="674" src="http://www.youtube.com/embed/rLDgQg6bq7o?rel=0" frameborder="0" allowfullscreen></iframe>
-			<br/><br/>
-			<p>Basically the page you are looking for is lost in a series of tubes.</p>
+	<h2>Not Found</h2>
+	<p>Sorry, but the page you were trying to view does not exist. Please allow Bud Haggart to explain.</p>
+	<iframe width="950" height="674" src="http://www.youtube.com/embed/rLDgQg6bq7o?rel=0" frameborder="0" allowfullscreen></iframe>
+	<p>Basically the page you are looking for is lost in a series of tubes.</p>
 
-	</div><!--/content-->
-</div><!--/page-->
+</div><!--/g12-->
 
-<?php include("footer.php"); ?>
+<?php include("../footer.php"); ?>
