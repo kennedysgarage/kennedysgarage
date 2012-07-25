d00e15b2f795fa1d6121c92437afde8d4ef4560f
Christopher Kennedy <kennedysgarage@gmail.com>
2012-07-25 08:03:57 -0400

    trying to add something about my websites that I build, still playing around with it

--- projects/projects.php
+++ projects/projects.php
@@ -1,7 +1,24 @@
 <?php  $title = 'Projects |'; include '../header.php'; ?>
 
-<h3>Logos</h3>
-<p>Here are some logos that I have designed for clients, employers, hackathons, or myself. Funny thing is I don't really call myself a logo designer or really care to do it. All of these logos just kind of just happened.</p>
+<div class="g12">
+    <h2>Latest Website</h2>
+    <p>Haven't quite figured out how I am going to layout the website section yet. So I am just testing the look and feel of this for now. Anyways, here is the latest website that I have built.</p>
+</div><!--/g12-->
+<div class="g8">
+    <img src="/projects/assets/projects/kristinastpeter.png" class="browser"/>
+</div><!--/g8-->
+<div class="g4">
+    <h3>Kristina St. Peter</h3>
+    <p><em>Design, HTML, &amp; CSS</em></p>
+    <p>Kristina works with me at Grade Spotter as Public Relations Coordinator. As a graduation gift I decided to build her a website. She is currently looking for a full-time job out in California.</p>
+    <p>Visit the website at <a href="http://kristinastpeter.com">kristinastpeter.com</a></p>
+</div><!--/g4-->
+
+<div class="g12">
+    <hr/>
+    <h2>Logos</h2>
+    <p>Here are some logos that I have designed for clients, employers, hackathons, or myself. Funny thing is I don't really call myself a logo designer or really care to do it. All of these logos just kind of just happened.</p>
+</div><!--/g12-->
 <div id="logos">
     <div class="g4">
         <img src="/projects/assets/logos/sutiandaxe.png"/>
