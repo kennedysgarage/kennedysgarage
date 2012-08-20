0080b6e8fa945272fc2d36b55f7b1e91810db747
Christoher Kennedy <kennedysgarage@gmail.com>
2012-08-20 19:53:16 -0400

    removing everything I moved to status

--- resume.php
+++ resume.php
@@ -9,54 +9,10 @@
 		</div><!--/g6-->
 		<div class="g6">
 			<h2>Christopher M. Kennedy</h2>
-			<p>I am currently not looking for work. I am happily employed at M. Shanken Communications as their Lead UX Strategist. Cranking out iPad apps, iPhone apps, Content Management Systems, internal tools, and public facing websites. I also keep myself extremely busy with a lot of side projects, hackathons, organizing events, meetups,  and conferences. </p>
+			<p>I am currently not looking for work. I am happily employed at M. Shanken Communications as their Lead UX Strategist. Cranking out iPad apps, iPhone apps, Content Management Systems, internal tools, and public facing websites. Checkout out a full list of all the projects, hackathons, events, speeches, honors, and what I have coming up on my <a href="/status">Status Board</a>.</p>
 		</div><!--/g6-->
 		
 		<hr/>
-		
-		<div class="g4">
-		    <h3><a href="http://techcrunch.com/events/disrupt-ny-2012/">TechCrunch Disrupt NYC</a></h3>
-		    <p>May 2012</p>
-		</div><!--/g4-->
-		<div class="g8">
-			<p>Hacker in New York, NY</p>
-    		<ul>
-    			<li>We won second place at the 2012 TechCrunch Disrupt Hackathon for building <a href="http://kennedysgarage.com/projects/poachbase">PoachBase.</a></li>
-    		</ul>
-		</div><!--/g8-->
-				
-		<div class="g4">
-		    <h3><a href="http://nyc.startupweekend.org/">Startup Weekend NYC</a></h3>
-		    <p>May 2012</p>
-		</div><!--/g4-->
-		<div class="g8">
-			<p>Mentor in New York, NY</p>
-    		<ul>
-    			<li>I was an UX coach &amp; mentor for the NYC Startup Weekend. I had a great time working with all the teams, everything from helping them understand their idea to working on their pitch.</li>
-    		</ul>
-		</div><!--/g8-->
-		
-		<div class="g4">
-		    <h3><a href="http://spaceappschallenge.org/">International Space Apps Challenge</a></h3>
-		    <p>April 2012</p>
-		</div><!--/g4-->
-		<div class="g8">
-			<p>Hacker in New York, NY</p>
-    		<ul>
-    			<li>Back to back weekend hackathons can normally wear someone out, not this guy. I will be hacking along some of the most talented and influential minds. We ended up creating <a href="http://predictthesky.herokuapp.com/">Predict the Sky</a> from the mission list.</li>
-    		</ul>
-		</div><!--/g8-->
-		
-		<div class="g4">
-		    <h3>HackerDen</h3>
-		    <p>April 2012</p>
-		</div><!--/g4-->
-		<div class="g8">
-			<p>Hacker in New York, NY</p>
-    		<ul>
-    			<li>I was privately invited to attend an elite hackathon for Spotify and that is all I can say about that at this time. I can say that I did build <a href="http://spotify.kennedysgarage.com/">Spotify Pattern</a> on the side, which is a pattern library to help myself and others quickly build out Spotify Apps.</li>
-    		</ul>
-		</div><!--/g8-->
 	
 		<div class="g4">
 		    <h3><a href="http://www.betabeat.com/2012/04/09/betabeats-spring-2012-most-poachable-players-in-tech/#slide22">Betabeat’s Spring 2012 Most Poachable Players in Tech</a></h3>
@@ -118,172 +74,7 @@
 			<li>Helping support and grow New York's hacker culture.</li>
 		</ul>
 	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://startupbus.com/">StartupBus NYC Hackathon</a></h3>
-		<p>February 2012</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Organized in New York, NY</p>
-		<ul>
-			<li>I was tasked with organizing our traditional Pre-Boarding Hackathon.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://thehackerolympics.com/">Hacker Olympics</a></h3>
-		<p>February 2012</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Attendee in New York, NY</p>
-		<ul>
-			<li>First hackathon I did not hack in. Helped with some very minor task to keep the show going.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://theeastwing.net/episodes/1-kennedy">The East Wing Podcast</a></h3>
-		<p>February 2012</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Tooting My Own Horn</p>
-		<ul>
-			<li>The podcast talks with industry experts about design, solving problems and the keys to creating products with value. I was on the first show, episode 1. I talked a little about what I have been up to, M. Shanken Communcations, 48 Hour Apps, The StartupBus, and Grade Spotter.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://generalassemb.ly/local-hack-day">Reinvent Local</a></h3>
-		<p>November 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Hacker in New York, NY</p>
-		<ul>
-			<li>Designed the overall look of the project for our team <a href="/projects/midpoint">Midpoint</a>. Spent most of my time hacking away in the terminal on the xml.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://photohackday.org">Photo Hack Day</a></h3>
-		<p>August 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Hacker in New York, NY</p>
-		<ul>
-			<li>Designed the overall look of the project for our team <a href="/projects/voronoi-diacam">Voronoi Diacam</a>. I won the food eating contest and the team won best use of Flickr's API.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://open.blogs.nytimes.com/2011/08/19/recapping-times-open-html5-and-beyond/">TimesOpen - HTML5 and Beyond</a></h3>
-		<p>August 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Attendee in New York, NY</p>
-		<ul>
-			<li>I had a front seat to listen to in-depth talks about HTML5 and related technologies. The speakers included <a href="http://twitter.com/#!/petele">Pete LePage</a>, <a href="http://twitter.com/#!/jashkenas">Jeremy Ashkenas</a> &amp; <a href="http://twitter.com/#!/davidpadbury">David Padbury</a>.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://ce1.com/intel/2011/nyappuplab/">NYC Intel AppUp Application Labs</a></h3>
-		<p>August 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Attendee in New York, NY</p>
-		<ul>
-			<li>Learned a lot about MeeGo OS and the Intel AppUp Store. Discussed with a few friends and hope to have a MeeGo based project in the works.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://googlecode.blogspot.com/2011/07/mark-your-calendars-html5-hackathons.htm">Google's HTML5 Web App Hackathon</a></h3>
-		<p>August 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Hacker in New York, NY</p>
-		<ul>
-			<li>Designed the logo and layout for our team <a href="/projects/transit-trainer">Transit Trainer</a>.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://forrst.com/">Forrst Meetup</a></h3>
-		<p>July 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Organized in Brooklyn, NY</p>
-		<ul>
-			<li>I organized the secound NYC Forrst Meetup.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://disrupt.techcrunch.com/NYC2011/">TechCrunch Disrupt Conference</a></h3>
-		<p>May 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Attendee in New York, NY</p>
-		<ul>
-			<li>It was nice to see a lot of highly respect individuals such as Tony Conrad, Ron Conway, Dennis Crowley, Paul Graham, Scott Harrison, Arianna Huffington, and much more.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://disrupt.techcrunch.com/NYC2011/hackathon/">TechCrunch Disrupt NYC</a></h3>
-		<p>May 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Hacker in New York, NY</p>
-		<ul>
-			<li>Designed the logo and Android UI for our team <a href="/projects/use-my-crm">My CRM</a> in New York City.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://techcrunch.com/tag/lemonade-stand/">TechCrunched</a></h3>
-		<p>April 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Tooting My Own Horn</p>
-		<ul>
-			<li>I have always looked up to TechCrunch and always wanted to be apart of it somehow. I never thought I would be on the front page.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://www.meetup.com/ny-tech/events/16791045/">Hack of the Month</a></h3>
-		<p>April 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Tooting My Own Horn</p>
-		<ul>
-			<li>We where honored to be Hack Of The Month at <a href="http://www.meetup.com/ny-tech/">NY Tech Meetup</a> for our impressive technical achievements for Lemonade Stand. Check out the video of us on stage in front of over 770+ people for <a href="http://youtu.be/DC-AiiclmV0">Hack of the Month</a>.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://forrstpodcast.com/post/4253501707/forrst-podcast-88-dinosaurs-and-aliens-interview">Forrst Podcast</a></h3>
-		<p>April 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Tooting My Own Horn</p>
-		<ul>
-			<li>The podcast covers the awesome Forrst community as well as the rest of the Internet and all things that are good in design and development. I was a guest on the 88th show, I talked a little about the StartupBus and some projects that I have been working on.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3>eBay Hangover Party and Speed Hack at SXSW</h3>
-		<p>March 2011</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Hacker in Austin, TX</p>
-		<ul>
-			<li>We integrated eBay's API in the newly created Lemonade Stand app for Android. We took home some pocket change for our hack.</li>
-		</ul>
-	</div><!--/g8-->
-	
+
 	<div class="g4">
 		<h3><a href="/projects/lemonade-stand">Lemonade Stand</a></h3>
 		<p>March 2011 - June 2011</p>
@@ -330,28 +121,6 @@
 		<ul>
 			<li>Taught introduction course to web design</li>
 			<li>Developed curriculum and class projects </li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3>Forrst Meetup</h3>
-		<p>November 2010 in Brooklyn, NY</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Organized in New York, NY</p>
-		<ul>
-			<li>I organized the first NYC Forrst Meetup.</li>
-		</ul>
-	</div><!--/g8-->
-	
-	<div class="g4">
-		<h3><a href="http://go.liverfoundation.org/">Liver Life Walk</a></h3>
-		<p>June 2010</p>
-	</div><!--/g4-->
-	<div class="g8">
-	    <p>Charity in New York, NY</p>
-		<ul>
-			<li>Raise a total of $1,050 for the Liver Life Walk to show support for my kid brother. Donations raised to sponsor the walk provided research, education, and advocacy to assist the 30 million people affected by liver disease in the United States.</li>
 		</ul>
 	</div><!--/g8-->
 	
