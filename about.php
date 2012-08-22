b095feac7a819bfbc8ecd16090f6ae96025ed7c7
Christoher Kennedy <kennedysgarage@gmail.com>
2012-08-22 12:50:19 -0400

    haha, thanks https://twitter.com/accohen/status/238313827383533568

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 
 	<div class="g8">
 		<h2>About Me</h2>
-	   	<p>Hello, my name Kennedy. I was recently named one of the Most <a href="http://betabeat.com/2012/04/betabeats-spring-2012-most-poachable-players-in-tech/#slide22">Poachable Players in Tech</a>, currently I lead the digital design front at M. Shanken Communications, where I have launched several successful apps and websites including an iPhone app that reached Top 3 in the App Store. I recently co&ndash;founded <a href="http://gradespotter.com">Grade Spotter</a>, a full-featured NYC restaurant inspection guide for the iPhone.</p>
+	   	<p>Hello, my name is Kennedy. I was recently named one of the Most <a href="http://betabeat.com/2012/04/betabeats-spring-2012-most-poachable-players-in-tech/#slide22">Poachable Players in Tech</a>, currently I lead the digital design front at M. Shanken Communications, where I have launched several successful apps and websites including an iPhone app that reached Top 3 in the App Store. I recently co&ndash;founded <a href="http://gradespotter.com">Grade Spotter</a>, a full-featured NYC restaurant inspection guide for the iPhone.</p>
 		<p>I have always been passionate about the overall user experience of the product and a keen eye for the details while keeping a clear vision of the big picture. Lately my main focus has been UX design for tablet &amp; mobile applications, however I have designed and launched several successful websites over the past eight years.</p>
 		<p>During my off hours I am very active in the startup and hackathon scene. Recently my team <a href="http://kennedysgarage.com/projects/poachbase">PoachBase</a> won second place at one of the world's largest hackathons put on by TechCrunch in NYC. While on the 2011 Startup Bus I was nicknamed "The Machine" for my hard work and dedication. I truly love what I do. There is always something exciting to learn and opportunities to help refine the craft.</p>
 		<p>I am also a Root member for <a href="http://www.hackerunion.org/">Hacker Union</a> <small>(fka NYHacker)</small> and a UX designer for 48HOURApps. You can often find me at <a href="http://nyhacker.org/">Hacker Townhall</a>, <a href="https://twitter.com/mdotHF">Mobile Hackers &amp; Founders</a>, <a href="https://twitter.com/#!/hackersfounders">Hackers &amp; Founders</a>, <a href="http://www.uxhappyhour.com/nyc">UX Happy Hour</a>, and <a href="http://www.meetup.com/ny-tech/">NY Tech Meetup</a>.</p>
@@ -14,7 +14,7 @@
 		<p>The original Kennedy&#8217;s Garage was open by my grandfather Theron Kennedy, in Lake of the Ozark, Missouri on February 28th, 1941. Granted 1941 was a different time and the business was building cars, nothing has changed as far as working hard. The key to his success was the relationships that he developed with his customers and the community.</p>
 		<p>Today I still value those traditions and enjoy the old fashion handshake. The original Kennedy&#8217;s Garage was shut down after the passing of Theron. I later reopened the name and wanted to keep the same values of my grandfather. Since then the family has moved away from building cars, we still have a deep passion for them, but now I build amazing ideas surrounding the internet.</p>
 	</div><!--/g8-->
-		
+
 	<div class="g4">
 		<figure>
 			<img src="/img/deep-inside.png" title="Photo Credit: @mturro"/>
