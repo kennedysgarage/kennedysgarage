13eafab0a2da81715c23d59a8f8ad4029632fd86
Christoher Kennedy <kennedysgarage@gmail.com>
2012-09-05 12:18:24 -0400

    fixed link to status chart

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 	    <nav>
 	    <ul>
 	        <li><a href="/about">About</a></li>
-	        <li><a href="/status">Status</a></li>
+	        <li><a href="https://www.statuschart.com/kennedysgarage">Status</a></li>
 			<li><a href="/projects">Projects</a></li>
 	        <li><a href="/articles">Articles</a></li>
 	    </ul>
