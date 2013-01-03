66b75116a04f9f6dacddc4e01f74e3b0c5ebcdcb
Christoher Kennedy <kennedysgarage@gmail.com>
2013-01-02 23:13:01 -0500

    adding a new thought

---
+++ articles/ux-idea-elevator-buttons.php
@@ -0,0 +1,14 @@
+<?php  $title = "UX Idea: Elevator Buttons |"; include '../header.php'; ?>
+
+<article>
+
+<h2>UX Idea: Elevator Buttons</h2>
+
+<p>I have a confession to make. I am that asshole that accidentally pushes the wrong button more often then I would like to admit. Whether it is the up/down buttons or somehow the wrong floor. I hate myself when I do it and I fell like a complete idiot.</p>
+<p>A simple solution for a simple mistake. You push the button to select your choice, why can't you just push it again to unselect your choice. </p>
+
+<p class="byline">January 27th, 2012 &there4; Kennedy</p>
+
+</article>
+
+<?php include("../footer.php"); ?>
