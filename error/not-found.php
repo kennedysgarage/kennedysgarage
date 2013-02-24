672f68fd6bab49e24672579ac3770296cb8db4c3
Christoher Kennedy <kennedysgarage@gmail.com>
2013-02-23 20:00:47 -0500

    clenaing up the 404 page

--- error/not-found.php
+++ error/not-found.php
@@ -1,12 +1,14 @@
 <?php  $title = 'Not Found |'; include '../header.php'; ?>
 
-<div class="g12">
-			
+<div id="page">
+  <div id="content">
+
 	<h2>Not Found</h2>
 	<p>Sorry, but the page you were trying to view does not exist. Please allow Bud Haggart to explain.</p>
 	<iframe width="950" height="674" src="http://www.youtube.com/embed/rLDgQg6bq7o?rel=0" frameborder="0" allowfullscreen></iframe>
 	<p>Basically the page you are looking for is lost in a series of tubes.</p>
 
-</div><!--/g12-->
+  </div><!--/content-->
+</div><!--/page-->
 
 <?php include("../footer.php"); ?>
