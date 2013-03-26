50a32d1c4ddb9dad89a2d2aa7d5298e48753f23f
Christoher Kennedy <kennedysgarage@gmail.com>
2013-03-26 13:16:32 -0400

    zip is much smaller

--- articles/iphone-template.php
+++ articles/iphone-template.php
@@ -5,7 +5,7 @@
 <h2>iPhone Template PSD</h2>
 <img src="/articles/assets/iphone-template/iphonetemplate.png"/>
 <p>I just got back from vacation and I took this photo while on the boat. I made it so it is supper easy to replace the screenshot with yours. This will work with both iPhone 4 &amp; 5, but keep in mind the bottom half is cut off in the picture. Feel free to use on any of your personal projects. Enjoy and have fun.</p>
-<a href="/articles/assets/iphone-template/iphonetemplate.psd" class="btn">Download PSD</a>
+<a href="/articles/assets/iphone-template/iphonetemplate.psd.zip" class="btn">Download PSD</a>
 
 <p class="byline">March 26th, 2013 &there4; Kennedy</p>
 
