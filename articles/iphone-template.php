671a2c316bbe3f6dba48d970f0d601e11aa0cfab
Christoher Kennedy <kennedysgarage@gmail.com>
2013-03-26 13:55:41 -0400

    adding a note about voting for status chart

--- articles/iphone-template.php
+++ articles/iphone-template.php
@@ -6,6 +6,8 @@
 <img src="/articles/assets/iphone-template/iphonetemplate.png"/>
 <p>I just got back from vacation and I took this photo while on the boat. I made it so it is supper easy to replace the screenshot with yours. This will work with both iPhone 4 &amp; 5, but keep in mind the bottom half is cut off in the picture. Feel free to use on any of your personal projects. Enjoy and have fun.</p>
 <a href="/articles/assets/iphone-template/iphonetemplate.psd.zip" class="btn">Download PSD</a>
+<br/><br/>
+<p class="highlight">If you enjoy it, please vote for <a href="https://www.statuschart.com">Status Chart</a> on BetaBeat's The Pitch page by clicking the "Like" button. <a href="http://betabeat.com/pitch/status-chart">VOTE NOW</a>!</p>
 
 <p class="byline">March 26th, 2013 &there4; Kennedy</p>
 
