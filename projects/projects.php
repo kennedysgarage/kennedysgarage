50758fa32b394e5d5f60d91d1fef37ecce40c98f
Christoher Kennedy <kennedysgarage@gmail.com>
2013-04-09 19:29:06 -0400

    updating the logo with some links

--- projects/projects.php
+++ projects/projects.php
@@ -14,27 +14,27 @@
 
 <div id="latest-work">
 <div class="g2">
-    <img src="/projects/assets/statuschart/statuschart.png" class="browser"/>
+    <img src="/projects/assets/statuschart/statuschart.png"/>
     <p><a href="https://www.statuschart.com/">Status Chart</a></p>
 </div><!--/g2-->
 <div class="g2">
-    <img src="/projects/assets/arctap/arctap.png" class="browser"/>
+    <img src="/projects/assets/arctap/arctap.png"/>
     <p><a href="http://arctap.com/">ArcTap</a></p>
 </div><!--/g2-->
 <div class="g2">
-    <img src="/projects/assets/gradespotter/gradespotter.png" class="browser"/>
+    <img src="/projects/assets/gradespotter/gradespotter.png"/>
     <p><a href="http://gradespotter.com/">Grade Spotter</a></p>
 </div><!--/g2-->
 <div class="g2">
-    <img src="/projects/assets/gradespotter/gradespotterapp.png" class="browser"/>
+    <img src="/projects/assets/gradespotter/gradespotterapp.png"/>
     <p><a href="http://gradespotter.com/">Grade Spotter App</a></p>
 </div><!--/g2-->
 <div class="g2">
-    <img src="/projects/assets/djkevinquinn/djkevinquinn.png" class="browser"/>
+    <img src="/projects/assets/djkevinquinn/djkevinquinn.png"/>
     <p><a href="http://djkevinquinn.com/">DJ Kevin Quinn</a></p>
 </div><!--/g2-->
 <div class="g2">
-    <img src="/projects/assets/kristinastpeter/kristinastpeter.png" class="browser"/>
+    <img src="/projects/assets/kristinastpeter/kristinastpeter.png"/>
     <p><a href="http://kristinastpeter.com">Kristina St. Peter</a></p>
 </div><!--/g2-->
 </div><!--/latest-work-->
@@ -46,64 +46,96 @@
 </div><!--/subtitle-->
 <div id="logos">
     <div class="g1">
+        <a href="/">
         <img src="/projects/assets/logos/kennedysgarage.png"/>
+        <p>Kennedy's Garage</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/majesticsquare.png"/>
+        <p>Majestic Square</p>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/labachtechnologylaw.png"/>
+        <p>Labach Technology Law</p>
     </div><!--/g1-->
     <div class="g1">
+        <a href="https://www.statuschart.com2">
         <img src="/projects/assets/logos/statuschart.png"/>
+        <p>Status Chart</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/theronindustries.png"/>
+        <p>Theron Industries</p>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/sutiandaxe.png"/>
+        <p>Suit &amp; Axe</p>
     </div><!--/g1-->
     <div class="g1">
+        <a href="http://arctap.com">
         <img src="/projects/assets/logos/arctap.png"/>
+        <p>ArcTap</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
+        <a href="http://gradespotter.com">
         <img src="/projects/assets/logos/gradespotter.png"/>
+        <p>Grade Spotter</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/mshanken.png"/>
+        <p>M. Shanken</p>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/adsperince.png"/>
+        <p>Adsperince</p>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/pixeloil.png"/>
+        <p>Pixel Oil</p>
     </div><!--/g1-->
     <div class="g1">
+        <a href="/projects/whoworksat">
         <img src="/projects/assets/logos/whoworksat.png"/>
+        <p>WhoWorks.at</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
+        <a href="/projects/in">
         <img src="/projects/assets/logos/in.png"/>
+        <p>In</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/hackerunion.png"/>
+        <p>Hacker Union</p>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/todaywegive.png"/>
+        <p>Today We Give</p>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/kqwebshow.png"/>
+        <p>KQ Webshow</p>
     </div><!--/g1-->
     <div class="g1">
+        <a href="/projects/lemonade-stand">
         <img src="/projects/assets/logos/lemonadestand.png"/>
+        <p>Lemonade Stand</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
+        <a href="/projects/use-my-crm">
         <img src="/projects/assets/logos/usemycrm.png"/>
+        <p>Use My CRM</p>
+        </a>
     </div><!--/g1-->
     <div class="g1">
         <img src="/projects/assets/logos/dbookshelf.png"/>
-    </div><!--/g1-->
-    <div class="g1">
-        <img src="/projects/assets/logos/kennedysgarage-old.png"/>
+        <p>dbookshelf</p>
     </div><!--/g1-->
 </div><!--/logos-->
 
