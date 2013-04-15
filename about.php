33bffbb29e046902a1fecf5a91bf7b7cf68306db
Kennedy <kennedysgarage@gmail.com>
2013-04-14 23:31:09 -0400

    cleaning up the copy to foucs on more of what I want to do

--- about.php
+++ about.php
@@ -3,12 +3,15 @@
 	<div id="page" class="clearfix about">
 
 		<div class="g4">
-		<img src="/img/home/face.png"/>
-	  <h2>About Me</h2>
-	  <p>I have always been <strong>passionate about the overall user experience</strong> of the product and a keen eye for the details while keeping a clear vision of the big picture. Lately my main focus has been UX design for tablet &amp; mobile applications, however I have designed and launched several successful websites over the past eight years.</p>
+		<img src="/img/deep-inside.jpg" title="Deep Inside by @mturro"/>
+	  <h2>The Mechanic</h2>
+	  <p>I'm Kennedy and I have always been <strong>passionate about the overall user experience</strong> of the product. I have a keen eye for the details while keeping a clear vision of the big picture. Lately my main focus has been UX Strategy &amp; Mobile UI Design, however I have designed and launched several successful websites over the past eight years.</p>
 	  <p>During my off hours I am very active in the startup and hackathon scene. Recently my team <a href="http://kennedysgarage.com/projects/poachbase">PoachBase</a> won second place at one of the world's largest hackathons put on by TechCrunch in NYC. While on the 2011 Startup Bus I was nicknamed "The Machine" for my hard work and dedication. I truly love what I do. There is always something exciting to learn and opportunities to help refine the craft.</p>
 	  <p>I am also a Root member for <a href="http://www.hackerunion.org/">Hacker Union</a> <small>(fka NYHacker)</small> and a UX designer for 48HOURApps. You can often find me at <a href="http://nyhacker.org/">Hacker Townhall</a>, <a href="https://twitter.com/mdotHF">Mobile Hackers &amp; Founders</a>, <a href="https://twitter.com/#!/hackersfounders">Hackers &amp; Founders</a>, <a href="http://www.uxhappyhour.com/nyc">UX Happy Hour</a>, and <a href="http://www.meetup.com/ny-tech/">NY Tech Meetup</a>.</p>
-	  <h3>Skills</h3>
+	  <h3>Problems I Solve</h3>
+	  <p><span class="highlight">Mobile &amp; Tablet Design.</span> You are an agency that needs a mercenary to quickly knock out that project. Or you're a startup that needs an MVP to show the investors. Either way I will hand craft your mobile design to fit the brand. We will start off discussing the overall goal and vision. From there we will make a few sketches then start getting the flow down in wireframes. After we agree that we are on the right path I will take everything into Photoshop to finalize the pixel-perfect design. The final step is to get those gorgeous assets to the developer. Don't worry, I won't leave you high and dry; I will be there for those last minute changes.</p>
+	  <h4>That's it?</h4>
+	  <p>Oh, I do more then just make pixel-perfect designs. Although my passion is designing mobile &amp; tablet apps, occasionally I do build websites. I believe in order to make a successful product you have to know more than just your job. I pride myself on the fact that I can have open and clear conversations with developers; communication is key.</p>
 	  <p>In no particular order, here is a little list of some of my skills: HTML5, CSS3, less, Markdown, Git, XML, JSON, iPhone design, iPad design, Android design, some JavaScript, some jQuery, some ROR, some PHP, some SEO, A/B testing, wireframes, rapid prototypes, and getting things off the top shelf. You can often find me working in Photoshop, Sublime Text 2, CodeKit, Terminal, and Vim. If you want to view my experiences, check out my <a href="/resume">r&eacute;sum&eacute;</a>.</p>
 	  </div><!--/g4-->
 
