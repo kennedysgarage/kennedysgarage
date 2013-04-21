a5052fe82bed575aee1b31f091c11be9d17fab6c
Kennedy <kennedysgarage@gmail.com>
2013-04-21 14:53:57 -0400

    trying out anther snippet

--- js/script.js
+++ js/script.js
@@ -16,11 +16,17 @@ $(document).ready(function(){
 		$('#filter-results ul li').show();
 	});
 
-	// http://stackoverflow.com/questions/2898740/iphone-safari-web-app-opens-links-in-new-window
-	$("a").click(function (event) {
-		event.preventDefault();
-		window.location = $(this).attr("href");
-	});
-
+	// http://stackoverflow.com/questions/8884376/iphone-safari-web-app-opens-links-in-new-window-when-using-javascripthistory-go
+	(function (a, b, c) {
+    if (c in b && b[c]) {
+        var d, e = a.location,
+            f = /^(a|html)$/i;
+        a.addEventListener("click", function (a) {
+            d = a.target;
+            while (!f.test(d.nodeName)) d = d.parentNode;
+            "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href);
+        }, !1);
+    }
+	})(document, window.navigator, "standalone");
 
 });
