4e547fa044a164fd06061108c23546c0ca9531a3
Kennedy <kennedysgarage@gmail.com>
2013-04-21 14:41:43 -0400

    trying out some js to open links

--- js/script.js
+++ js/script.js
@@ -16,6 +16,17 @@ $(document).ready(function(){
 		$('#filter-results ul li').show();
 	});
 
+	// http://stackoverflow.com/questions/2898740/iphone-safari-web-app-opens-links-in-new-window
+	var a=document.getElementsByTagName("a");
+	for(var i=0;i<a.length;i++)
+	{
+	  a[i].onclick=function()
+	  {
+	      window.location=this.getAttribute("href");
+	      return false
+	  }
+	}
+
 
 
 });
