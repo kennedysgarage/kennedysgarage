c392f4500fcb01c0178c8b83306b8f9fd6dfb7df
Kennedy <kennedysgarage@gmail.com>
2013-06-13 13:06:18 -0400

    this is my normal process, need ot add more info to it

---
+++ process.php
@@ -0,0 +1,87 @@
+<?php  $title = 'Process | '; include 'header.php'; ?>
+
+  <section id="intro">
+    <div class="container">
+      <h2>Process</h2>
+    </div><!--/container-->
+  </section>
+  <div class="container">
+    <div class="page">
+      <div class="content">
+        <div class="g4">
+          <p>In general this is my normal process.</p>
+          <ol>
+            <li><strong>Discovery</strong>
+              <ul>
+                <li>Learning business objectives</li>
+                <li>Prioritize features</li>
+                <li>View past statistics</li>
+                <li>User Research</li>
+                <li>User Stories and Personas</li>
+                <li>Brainstorming session</li>
+              </ul>
+            </li>
+            <li><strong>Planning</strong>
+              <ul>
+                <li>Information architecture</li>
+                <li>Build a site-map</li>
+                <li>Discuss navigation</li>
+                <li>Project milestones</li>
+              </ul>
+            </li>
+            <li><strong>Wireframing</strong>
+              <ul>
+                <li>Basic sketches</li>
+                <li>Prototype</li>
+                <li>Test layout options</li>
+                <li>Construct visual hierarchy</li>
+              </ul>
+            </li>
+            <li><strong>Design</strong>
+              <ul>
+                <li>Color, images, &amp; copy integrated</li>
+              </ul>
+            </li>
+            <li><strong>Front-end Coding</strong>
+              <ul>
+                <li>HTML, CSS, JS</li>
+              </ul>
+            </li>
+            <li><strong>Back-end Coding</strong>
+              <ul>
+                <li>Integrate CMS</li>
+              </ul>
+            </li>
+            <li><strong>Test</strong>
+              <ul>
+                <li>Deploy to beta</li>
+                <li>Usability</li>
+                <li>Load</li>
+                <li>Accessibility</li>
+                <li>Train users</li>
+              </ul>
+            </li>
+            <li><strong>Deploy</strong>
+              <ul>
+                <li>Deploy to production</li>
+              </ul>
+            </li>
+            <li><strong>Review</strong>
+              <ul>
+                <li>Establish a road-map for improvements</li>
+                <li>Analytics</li>
+              </ul>
+            </li>
+            <li><strong>Manage</strong>
+              <ul>
+                <li>Execute improvements</li>
+              </ul>
+            </li>
+          </ol>
+        </div><!--/g4-->
+
+      </div><!--/content-->
+    </div><!--/page-->
+  </div><!--/container-->
+
+<?php include("footer.php"); ?>
