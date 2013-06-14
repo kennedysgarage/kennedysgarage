c42fff917d0d54a080a899a441e9428f86f9f8b4
Kennedy <kennedysgarage@gmail.com>
2013-06-14 17:05:33 -0400

    adding some base files for a possible labs section

---
+++ labs/labs.php
@@ -0,0 +1,41 @@
+<?php  $title = 'Labs |'; include '../header.php'; ?>
+
+<div class="projects">
+    <section id="intro">
+        <div class="container">
+            <h2>The Workbench</h2>
+            <!-- <p>It is kind of messy right now, currently trying to figure out the best way to show off my work. I love designing iPhone apps and enjoy designing &amp; building websites. My main goal is to give the users a great experience.</p> -->
+        </div><!--container-->
+    </section>
+    <div class="container">
+        <div class="page">
+            <section class="content">
+                <div id="latest">
+
+                    <div class="g1_5">
+                        <a href="/projects/bring-your-own-magnum">
+                        <img src="/img/projects/bring-your-own-magnum.png"/>
+                        <p><strong>Bring Your Own Magnum</strong> Designer</p>
+                        </a>
+                    </div>
+                    <div class="g1_5">
+                        <a href="/projects/ro">
+                        <img src="/img/projects/ro.png"/>
+                        <p><strong>RO</strong> Logo</p>
+                        </a>
+                    </div>
+                    <div class="g1_5">
+                        <a href="/projects/startupbus">
+                        <img src="/img/projects/startupbus.png"/>
+                        <p><strong>StartupBus</strong> Foursquare Badge</p>
+                        </a>
+                    </div>
+
+                </div><!--latest-->
+
+            </section>
+        </div><!--page-->
+    </div><!--container-->
+</div><!--projects-->
+
+<?php include("../footer.php"); ?>
