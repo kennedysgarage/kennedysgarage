b5086bc7785c21d08734c9d63143c0ff91a2786f
Kennedy <kennedysgarage@gmail.com>
2013-11-02 02:19:54 -0400

    converting all the php includes to jekyll front matter

---
+++ _site/assets/js/plugins.js
@@ -0,0 +1,15 @@
+//@codekit-prepend "ss-social.js";
+//@codekit-prepend "ss-standard.js";
+//@codekit-prepend "fitvids.js";
+//@codekit-prepend "prettify.js";
+
+$(document).ready(function(){
+
+    // To make the code look beautiful for you
+    // http://code.google.com/p/google-code-prettify/
+    prettyPrint();
+
+    // Basic FitVids Test
+    $(".container").fitVids();
+
+});
--- status.php
+++
@@ -1,7 +0,0 @@
-<?php  $title = 'Status |'; include 'header.php'; ?>
-
-<script type="text/javascript">
-window.location = "https://www.statuschart.com/kennedysgarage";
-</script>
-
-<?php include("footer.php"); ?>
