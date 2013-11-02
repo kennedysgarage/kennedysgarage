c89dbfe17da5b3289426d4b9a7ab3e2e698abebc
Kennedy <kennedysgarage@gmail.com>
2013-11-02 04:38:49 -0400

    setting up jekyll assets with a little help from @ttimsmith http://timlikestoteach.com/timtv/using-the-jekyll-assets-plugin

---
+++ _assets/javascripts/plugins.js
@@ -0,0 +1,15 @@
+//= require vendor/ss-social
+//= require vendor/ss-standard
+//= require vendor/fitvids
+//= require vendor/prettify
+
+$(document).ready(function(){
+
+    // To make the code look beautiful for you
+    // http://code.google.com/p/google-code-prettify/
+    prettyPrint();
+
+    // Basic FitVids Test
+    $(".container").fitVids();
+
+});
---
+++ _assets/javascripts/script.js
@@ -0,0 +1,19 @@
+//= require plugins
+
+$(document).ready(function(){
+
+	// http://web.enavu.com/tutorials/client-side-filtering-data-with-jquery/
+	$('#filters a').click(function(e){
+		e.preventDefault();
+		var filter = $(this).attr('id');
+		$('#filter-results ul li').show();
+		$('#filter-results ul li:not(.' + filter + ')').hide();
+		e.preventDefault();
+	});
+	$('#all').click(function(e){
+		e.preventDefault();
+		var filter = $(this).attr('id');
+		$('#filter-results ul li').show();
+	});
+
+});
