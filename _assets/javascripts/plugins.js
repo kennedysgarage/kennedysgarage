084e1a1c9744fd25f89392bc42a51eee8337e874
Kennedy <kennedysgarage@gmail.com>
2013-12-02 01:47:46 -0500

    no long usering this

--- _assets/javascripts/plugins.js
+++ _assets/javascripts/plugins.js
@@ -4,10 +4,6 @@
 
 $(document).ready(function(){
 
-    // To make the code look beautiful for you
-    // http://code.google.com/p/google-code-prettify/
-    prettyPrint();
-
     // Basic FitVids Test
     $(".container").fitVids();
 
