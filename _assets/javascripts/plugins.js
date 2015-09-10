5daffafd77b23c234f87142a8149534a343e0231
kennedy <kennedysgarage@gmail.com>
2015-09-10 15:35:41 -0400

    adding a filter for the projects based on @swcannizzaro suggestion

--- _assets/javascripts/plugins.js
+++ _assets/javascripts/plugins.js
@@ -1,10 +1,14 @@
 //= require vendor/ss-standard
 //= require vendor/ss-social
 //= require vendor/fitvids
+//= require vendor/mixitup
 
 $(document).ready(function(){
 
     // Basic FitVids Test
     $(".container").fitVids();
+
+    // MixitUp by KunkaLabs
+    $('#pros').mixItUp();
 
 });
